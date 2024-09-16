d933ea2114dee0e0fc3f553d720dc0be4d4b2899
Eslam <Samehselem22@gmail.com>
2024-09-17 00:29:22 +0300

    Refactor TextMatcher module and utils.py
    - Remove unused import in TextMatcher.py
    - Add TextMatcher import in __init__.py
    - Add golden_section function in utils.py
    - Update MANIFEST.in to include new files

--- ArabicTagger/__init__.py
+++ ArabicTagger/__init__.py
@@ -6,7 +6,8 @@ from .CRF    import CRF
 from .NER    import NER
 from .Tagger import Tagger
 from .utils  import save, load
-
+from .TextMatcher import TextMatcher
+from .utils import golden_section
 
 
 
